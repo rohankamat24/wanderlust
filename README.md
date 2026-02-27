8715da9cccc7e17a663d25feaa02cd22adf52562
rohankamat24 <rohankamat87000@gmail.com>
2026-02-27 19:13:07 +0530

    Basic Setup (Modified app.js, README.md)

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## ⚙️ Installation
--> npm init -y
--> npm i express 
--> npm i ejs 
--> npm i mongoose 
+npm init -y
+npm i express 
+npm i ejs 
+npm i mongoose 
+npm install dotenv
--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+if (process.env.NODE_ENV != "production") {
+    require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+
+const MONGO_URL = process.env.MONGODB_URL;
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
 
 app.listen(8080, () => {
   console.log("server is listening to port 8080");
