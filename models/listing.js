11d244a826e81fccde16f680e5400b794a585531
rohankamat24 <rohankamat87000@gmail.com>
2026-04-05 22:04:04 +0530

    Implemented Edit listing Image, Image preview in edit page, Implemented map using mapbox, used geocoding to store coordinates of location in listing in DB(geoJSON), Add Map Marker in map via mapbox, also Popup message in mapvia mapbox ( Add map.js, Modified listing.js in controller models routes, style.css, boilerplate.ejs, edit.ejs, new.ejs, show.ejs )

--- models/listing.js
+++ models/listing.js
@@ -25,11 +25,22 @@ const listingSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+  geometry: {
+    type: {
+      type: String, // Don't do `{ location: { type: String } }`
+      enum: ["Point"], // 'location.type' must be 'Point'
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
 });
 
 listingSchema.post("findOneAndDelete", async (listing) => {
   if (listing) {
-    await Review.deleteMany({ _id:  { $in: listing.reviews } });
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
   }
 });
 
